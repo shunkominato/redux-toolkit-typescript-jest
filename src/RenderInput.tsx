579b72953320559311d1a5cc194ecf51069472a9
erika9099 <shun19871001@i.softbank.jp>
2021-04-20 08:52:07 +0900

    inputcomponent add

---
+++ src/RenderInput.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const RenderInput = () => {
+  const [input, setInput] = useState('');
+
+  const updateValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='enter'
+        value={input}
+        onChange={updateValue}
+      />
+    </div>
+  )
+}
+
+export default RenderInput
